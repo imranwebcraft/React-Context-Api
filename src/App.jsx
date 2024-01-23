fac30d36f50539f90d7f9466e23d2515d821b1fd
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-23 15:24:13 +0600

    Understand the context api

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,25 @@ import Section from "./components/section";
 
 export default function Page() {
 	return (
-		<Section>
-			<Heading>Title</Heading>
+		<>
 			<Section>
-				<Heading>Heading</Heading>
-				<Heading>Heading</Heading>
-				<Heading>Heading</Heading>
+				<Heading>Title</Heading>
 				<Section>
-					<Heading>Sub-heading</Heading>
-					<Heading>Sub-heading</Heading>
-					<Heading>Sub-heading</Heading>
+					<Heading>Heading</Heading>
+					<Heading>Heading</Heading>
+					<Heading>Heading</Heading>
 					<Section>
-						<Heading>Sub-sub-heading</Heading>
-						<Heading>Sub-sub-heading</Heading>
-						<Heading>Sub-sub-heading</Heading>
+						<Heading>Sub-heading</Heading>
+						<Heading>Sub-heading</Heading>
+						<Heading>Sub-heading</Heading>
+						<Section>
+							<Heading>Sub-sub-heading</Heading>
+							<Heading>Sub-sub-heading</Heading>
+							<Heading>Sub-sub-heading</Heading>
+						</Section>
 					</Section>
 				</Section>
 			</Section>
-		</Section>
+		</>
 	);
 }
