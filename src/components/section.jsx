836e53506af3dc9ef179a55a00ab6fce0edba1dd
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-23 15:13:07 +0600

    simplify the section component by managing level from section component without passing level props

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import { LevelContext } from "../context/LevelContext";
 
-export default function Section({ level, children }) {
+export default function Section({ children }) {
+	const level = useContext(LevelContext);
+	// level er value update hobe every render a. And seta store hobe initial value hisabe. Porer render a sei value e use hobe and seatr saathei 1 plus hobe
 	return (
 		<section className="section">
-			<LevelContext.Provider value={level}>{children}</LevelContext.Provider>
+			<LevelContext.Provider value={level + 1}>
+				{children}
+			</LevelContext.Provider>
 		</section>
 	);
 }
